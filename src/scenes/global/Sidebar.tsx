a52459300521647987763fdda67b19a3b4e6a67a
juaamol <jambulud@gmail.com>
2022-12-29 12:32:22 +0100

    sidebar

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -1,5 +1,217 @@
-import React from 'react';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import {
+  Sidebar as ProSidebar,
+  Menu,
+  MenuItem,
+  sidebarClasses,
+  menuClasses,
+  useProSidebar,
+} from 'react-pro-sidebar';
+import reactLogo from '../../assets/react.svg';
+import { tokens } from '../../theme';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import { useState } from 'react';
+import { SidebarItem } from './SidebarItem';
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const { collapseSidebar, collapsed: isCollapsed } = useProSidebar();
+  const [selected, setSelected] = useState('Dashboard');
+  return (
+    <Box>
+      <ProSidebar
+        defaultCollapsed={false}
+        rootStyles={{
+          [`.${sidebarClasses.container}`]: {
+            backgroundColor: colors.primary[400],
+          },
+          '&': {
+            borderRight: 0,
+          },
+        }}
+      >
+        <Menu
+          menuItemStyles={{
+            button: ({ active }) => ({
+              backgroundColor: 'transparent',
+              color: active ? '#6870fa !important' : undefined,
+            }),
+          }}
+          rootStyles={{
+            [`.${menuClasses.button}:hover`]: {
+              backgroundColor: '#868dfb',
+            },
+          }}
+        >
+          <MenuItem
+            onClick={() => collapseSidebar(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: '10px 0 20px 0',
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+                ml='15px'
+              >
+                <Typography variant='h3' color={colors.grey[100]}>
+                  ADMIN
+                </Typography>
+                <IconButton onClick={() => collapseSidebar(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                <img
+                  alt='profile-user'
+                  width='100px'
+                  height='100px'
+                  src={reactLogo}
+                  style={{ cursor: 'pointer', borderRadius: '50%' }}
+                />
+              </Box>
+              <Box textAlign='center'>
+                <Typography
+                  variant='h2'
+                  color={colors.grey[100]}
+                  fontWeight='bold'
+                  sx={{ m: '10px 0 0 0' }}
+                >
+                  Username
+                </Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>
+                  Role
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box>
+            <SidebarItem
+              title='Dashboard'
+              to='/'
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant='h6'
+              color={colors.grey[300]}
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Data
+            </Typography>
+            <SidebarItem
+              title='Manage Team'
+              to='/team'
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Contacts Information'
+              to='/contacts'
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Invoices Balances'
+              to='/invoices'
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant='h6'
+              color={colors.grey[300]}
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Pages
+            </Typography>
+            <SidebarItem
+              title='Profile Form'
+              to='/form'
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Calendar'
+              to='/calendar'
+              icon={<CalendarTodayOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='FAQ Page'
+              to='/faq'
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant='h6'
+              color={colors.grey[300]}
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Charts
+            </Typography>
+            <SidebarItem
+              title='Bar Chart'
+              to='/bar'
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Pie Chart'
+              to='/pie'
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Line Chart'
+              to='/line'
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <SidebarItem
+              title='Geography Chart'
+              to='/geography'
+              icon={<MapOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
 };
---
+++ src/scenes/global/SidebarItem.tsx
@@ -0,0 +1,32 @@
+import { Typography, useTheme } from '@mui/material';
+import { FC, ReactNode } from 'react';
+import { MenuItem } from 'react-pro-sidebar';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
+
+interface SidebarItemProps {
+  title: string;
+  to: string;
+  icon: ReactNode;
+  selected: string;
+  setSelected: (title: string) => void;
+}
+
+export const SidebarItem: FC<SidebarItemProps> = (props) => {
+  const { title, to, icon, selected, setSelected } = props;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
